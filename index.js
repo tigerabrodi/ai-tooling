8e1795916dc67f99052db79ea74d7f4910d3a5f9
Tiger Abrodi <tigerabrodi@gmail.com>
2024-05-11 20:22:25 +0200

    commit

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ import 'dotenv/config'
 
 import OpenAI from 'openai'
 const openai = new OpenAI()
+
+openai.chat.completions.create({
+  model: 'gpt-3.5-turbo',
+  messages: [
+    {
+      role: '',
+      content:
+        'You are an AI assistant, answer any questions to the best of your ability',
+    },
+  ],
+})
